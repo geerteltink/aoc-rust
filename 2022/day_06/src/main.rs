8b1a9a23d38e7d3942631e5af2724543676b6845
Geert Eltink <geert.eltink@gmail.com>
2022-12-06 07:48:41 +0100

    feat: solution 2022 day 6

--- 2022/day_06/src/main.rs
+++ 2022/day_06/src/main.rs
@@ -1,5 +1,7 @@
 static DAY: &'static str = "06";
 
+use itertools::Itertools;
+
 fn main() {
     let input = std::fs::read_to_string(format!("./2022/day_{DAY}/fixtures/input.txt")).unwrap();
 
@@ -10,29 +12,43 @@ fn main() {
     println!("Answer day {DAY} part one: {result2}");
 }
 
-fn part_one(_input: &String) -> i32 {
-    return 0;
+fn part_one(input: &String) -> usize {
+    let message = input.chars().collect_vec();
+    
+    return get_start_of_packet(message, 4);
 }
 
-fn part_two(_input: &String) -> i32 {
-    return 0;
+fn part_two(input: &String) -> usize {
+    let message = input.chars().collect_vec();
+    
+    return get_start_of_packet(message, 14);
+}
+
+fn get_start_of_packet(message: Vec<char>, packet_size: usize) -> usize {
+    let mut count = 0;
+    for marker in message.windows(packet_size) { // get slice of x characters, if smaller than x, it is ignored
+        if marker.iter().all_unique() { // check if all characters are unique
+            break;
+        }
+        count += 1;
+    }
+    
+    return count + packet_size;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[ignore]
     #[test]
     fn it_returns_the_answer_for_part_one() {
         let input = std::fs::read_to_string("./fixtures/input_test.txt").unwrap();
-        assert_eq!(0, part_one(&input));
+        assert_eq!(10, part_one(&input));
     }
 
-    #[ignore]
     #[test]
     fn it_returns_the_answer_for_part_two() {
         let input = std::fs::read_to_string("./fixtures/input_test.txt").unwrap();
-        assert_eq!(0, part_two(&input));
+        assert_eq!(29, part_two(&input));
     }
 }
