1fc3e38b89e078488d9a3d8d905c0b492f2636db
Geert Eltink <geert.eltink@gmail.com>
2022-12-12 16:43:11 +0100

    feat: prepare 2022 day 13

---
+++ 2022/day_13/src/main.rs
@@ -0,0 +1,43 @@
+#![allow(unused_imports)]
+
+use default::*;
+use grid::*;
+
+static DAY: &'static str = "13";
+
+fn main() {
+    let input = std::fs::read_to_string(format!("./2022/day_{DAY}/fixtures/input.txt")).unwrap();
+
+    let result1 = part_one(&input);
+    println!("Answer day {DAY} part one: {result1}");
+
+    let result2 = part_two(&input);
+    println!("Answer day {DAY} part one: {result2}");
+}
+
+fn part_one(_input: &String) -> i32 {
+    return 0;
+}
+
+fn part_two(_input: &String) -> i32 {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[ignore]
+    #[test]
+    fn it_returns_the_answer_for_part_one() {
+        let input = std::fs::read_to_string("./fixtures/input_test.txt").unwrap();
+        assert_eq!(0, part_one(&input));
+    }
+
+    #[ignore]
+    #[test]
+    fn it_returns_the_answer_for_part_two() {
+        let input = std::fs::read_to_string("./fixtures/input_test.txt").unwrap();
+        assert_eq!(0, part_two(&input));
+    }
+}
