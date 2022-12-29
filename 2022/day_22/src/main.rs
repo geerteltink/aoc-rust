0859aa5cd7c4e4b711f220575b618c73305670ac
Geert Eltink <geert.eltink@gmail.com>
2022-12-29 07:50:56 +0100

    refactor: use the new grid lib

--- 2022/day_22/src/main.rs
+++ 2022/day_22/src/main.rs
@@ -1,10 +1,10 @@
-use aoc::arena::*;
+use aoc::grid::*;
 use aoc::*;
 
 static DAY: &'static str = "22";
 
-const OUT_OF_BOUNDS: i8 = b' ' as i8;
-const WALL: i8 = b'#' as i8;
+const OUT_OF_BOUNDS: char = ' ';
+const WALL: char = '#';
 
 #[derive(Debug)]
 enum Instruction {
@@ -23,7 +23,7 @@ enum Direction {
 
 #[derive(Debug)]
 struct Player {
-    loc: Loc,
+    loc: Coordinate,
     dir: Direction,
 }
 
@@ -46,24 +46,24 @@ impl Player {
         }
     }
 
-    fn walk(&mut self, x: usize, board: &DefaultHashMap<Loc, i8>) {
+    fn walk(&mut self, x: usize, board: &Grid<char>) {
         for _ in 0..x {
             let mut new_loc = self.loc;
             match self.dir {
-                Direction::Up => new_loc += Loc::new(0, -1),
-                Direction::Down => new_loc += Loc::new(0, 1),
-                Direction::Right => new_loc += Loc::new(1, 0),
-                Direction::Left => new_loc += Loc::new(-1, 0),
+                Direction::Up => new_loc += Coordinate::new(0, -1),
+                Direction::Down => new_loc += Coordinate::new(0, 1),
+                Direction::Right => new_loc += Coordinate::new(1, 0),
+                Direction::Left => new_loc += Coordinate::new(-1, 0),
             }
 
-            let board_loc = board.get(new_loc);
-            if board_loc == &WALL {
+            let board_loc = board[new_loc];
+            if board_loc == WALL {
                 //println!("Hit wall {:?}", &new_loc);
                 // do nothing if a wall is hit
                 return;
             }
 
-            if board_loc == &OUT_OF_BOUNDS {
+            if board_loc == OUT_OF_BOUNDS {
                 // continue on the other side of the board
                 match self.dir {
                     Direction::Up => {
@@ -104,82 +104,8 @@ impl Player {
                     }
                 }
 
-                let board_loc = board.get(new_loc);
-                if board_loc == &WALL {
-                    //println!("Hit wall on the other side{:?}", &new_loc);
-                    // do nothing if a wall is hit
-                    return;
-                }
-
-                //println!("Out of bounds {:?}", &new_loc);
-            }
-
-            //println!("Moved {:?} from {:?} -> {:?}", &self.dir, old_loc, &new_loc);
-
-            self.loc = new_loc;
-        }
-    }
-    
-    fn walk_in_cube(&mut self, x: usize, board: &DefaultHashMap<Loc, i8>) {
-        for _ in 0..x {
-            let mut new_loc = self.loc;
-            match self.dir {
-                Direction::Up => new_loc += Loc::new(0, -1),
-                Direction::Down => new_loc += Loc::new(0, 1),
-                Direction::Right => new_loc += Loc::new(1, 0),
-                Direction::Left => new_loc += Loc::new(-1, 0),
-            }
-
-            let board_loc = board.get(new_loc);
-            if board_loc == &WALL {
-                //println!("Hit wall {:?}", &new_loc);
-                // do nothing if a wall is hit
-                return;
-            }
-            
-            if board_loc == &OUT_OF_BOUNDS {
-                // continue on the next side inside the cube
-                match self.dir {
-                    Direction::Up => {
-                        new_loc = board
-                            .iter()
-                            .filter(|(loc, value)| loc.x == new_loc.x && value != &&OUT_OF_BOUNDS)
-                            .max_by(|a, b| a.0.y.cmp(&b.0.y))
-                            .unwrap()
-                            .0
-                            .to_owned()
-                    }
-                    Direction::Down => {
-                        new_loc = board
-                            .iter()
-                            .filter(|(loc, value)| loc.x == new_loc.x && value != &&OUT_OF_BOUNDS)
-                            .min_by(|a, b| a.0.y.cmp(&b.0.y))
-                            .unwrap()
-                            .0
-                            .to_owned()
-                    }
-                    Direction::Right => {
-                        new_loc = board
-                            .iter()
-                            .filter(|(loc, value)| loc.y == new_loc.y && value != &&OUT_OF_BOUNDS)
-                            .min_by(|a, b| a.0.x.cmp(&b.0.x))
-                            .unwrap()
-                            .0
-                            .to_owned()
-                    }
-                    Direction::Left => {
-                        new_loc = board
-                            .iter()
-                            .filter(|(loc, value)| loc.y == new_loc.y && value != &&OUT_OF_BOUNDS)
-                            .max_by(|a, b| a.0.x.cmp(&b.0.x))
-                            .unwrap()
-                            .0
-                            .to_owned()
-                    }
-                }
-
-                let board_loc = board.get(new_loc);
-                if board_loc == &WALL {
+                let board_loc = board[new_loc];
+                if board_loc == WALL {
                     //println!("Hit wall on the other side{:?}", &new_loc);
                     // do nothing if a wall is hit
                     return;
@@ -227,20 +153,12 @@ fn main() {
     println!("Answer day {DAY} part two: {result2} in {:?}", end - start);
 }
 
-fn part_one(input: &String) -> i64 {
-    let mut board: DefaultHashMap<Loc, i8> = DefaultHashMap::new(OUT_OF_BOUNDS);
-
-    // add all non empty spaces to the board
+fn part_one(input: &String) -> isize {
     let (board_rows, directions) = input.split_once("\n\n").unwrap();
-    for (y, board_row) in board_rows.lines().enumerate() {
-        for (x, c) in board_row.chars().enumerate() {
-            let c = c as i8;
-            if c != OUT_OF_BOUNDS {
-                board[Loc::new((x + 1) as i64, (y + 1) as i64)] = c;
-            }
-        }
-    }
+    let board = Grid::from_input(board_rows, |c| c, OUT_OF_BOUNDS);
 
+    //board.print();
+    
     // convert directions to instructions
     let mut instructions = Vec::new();
     let mut num = 0;
@@ -267,7 +185,7 @@ fn part_one(input: &String) -> i64 {
     // find player start position
     let player_start = board
         .iter()
-        .filter(|(loc, value)| loc.y == 1 && value != &&OUT_OF_BOUNDS)
+        .filter(|(loc, value)| loc.y == 0 && value != &&OUT_OF_BOUNDS)
         .min_by(|a, b| a.0.x.cmp(&b.0.x))
         .unwrap()
         .0
@@ -286,77 +204,15 @@ fn part_one(input: &String) -> i64 {
             Instruction::Move(x) => player.walk(x, &board),
         }
     }
-
+    
     // The final password is the sum of 1000 times the row, 4 times the column, and the facing.
-    let answer = player.loc.y * 1000 + player.loc.x * 4;
+    let answer = (player.loc.y + 1) * 1000 + (player.loc.x + 1) * 4;
 
     return answer;
 }
 
-fn part_two(input: &String) -> i64 {
-    let mut board: DefaultHashMap<Loc, i8> = DefaultHashMap::new(OUT_OF_BOUNDS);
-
-    // add all non empty spaces to the board
-    let (board_rows, directions) = input.split_once("\n\n").unwrap();
-    for (y, board_row) in board_rows.lines().enumerate() {
-        for (x, c) in board_row.chars().enumerate() {
-            let c = c as i8;
-            if c != OUT_OF_BOUNDS {
-                board[Loc::new((x + 1) as i64, (y + 1) as i64)] = c;
-            }
-        }
-    }
-
-    // convert directions to instructions
-    let mut instructions = Vec::new();
-    let mut num = 0;
-    for c in directions.trim().chars() {
-        if c.is_ascii_digit() {
-            num = num * 10 + c.to_digit(10).unwrap();
-        } else {
-            if num != 0 {
-                instructions.push(Instruction::Move(num as usize));
-                num = 0;
-            }
-            match c {
-                'R' => instructions.push(Instruction::TurnRight),
-                'L' => instructions.push(Instruction::TurnLeft),
-                _ => unreachable!(),
-            }
-        }
-    }
-    // add a possible left over movement to the instructions
-    if num != 0 {
-        instructions.push(Instruction::Move(num as usize));
-    }
-
-    // find player start position
-    let player_start = board
-        .iter()
-        .filter(|(loc, value)| loc.y == 1 && value != &&OUT_OF_BOUNDS)
-        .min_by(|a, b| a.0.x.cmp(&b.0.x))
-        .unwrap()
-        .0
-        .to_owned();
-
-    let mut player = Player {
-        loc: player_start,
-        dir: Direction::Right,
-    };
-
-    // I like to move it, move it
-    for instruction in instructions {
-        match instruction {
-            Instruction::TurnLeft => player.turn_left(),
-            Instruction::TurnRight => player.turn_right(),
-            Instruction::Move(x) => player.walk(x, &board),
-        }
-    }
-
-    // The final password is the sum of 1000 times the row, 4 times the column, and the facing.
-    let answer = player.loc.y * 1000 + player.loc.x * 4;
-
-    return answer;
+fn part_two(_input: &String) -> i64 {
+    return 0;
 }
 
 #[cfg(test)]
@@ -370,18 +226,18 @@ mod tests {
         let input = load_input("./fixtures/input_test.txt");
         assert_eq!(6032, part_one(&input));
     }
+    
+    #[test]
+    fn it_returns_the_answer_for_part_one() {
+        let input = load_input("./fixtures/input.txt");
+        assert_eq!(56372, part_one(&input));
+    }
 
     #[ignore]
     #[test]
     fn it_returns_the_test_answer_for_part_two() {
         let input = load_input("./fixtures/input_test.txt");
         assert_eq!(5031, part_two(&input));
-    }
-
-    #[test]
-    fn it_returns_the_answer_for_part_one() {
-        let input = load_input("./fixtures/input.txt");
-        assert_eq!(56372, part_one(&input));
     }
 
     #[ignore]
